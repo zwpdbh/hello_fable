837cc7108f14085ee0f80298acca908c46882f64
zwpdbh <hyperion_z@outlook.com>
2023-06-09 14:16:46 +0800

    <update init > implement sheet

--- src/App.fs
+++ src/App.fs
@@ -6,9 +6,11 @@ open Browser.Dom
 let mutable count = 0
 
 // Get a reference to our button and cast the Element to an HTMLButtonElement
-let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
+let myButton =
+  document.querySelector (".my-button") :?> Browser.Types.HTMLButtonElement
 
 // Register our listener
-myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "You clicked: %i time(s)" count
+myButton.onclick <-
+  fun _ ->
+    count <- count + 2
+    myButton.innerText <- sprintf "You clicked www: %i time(s)" count
